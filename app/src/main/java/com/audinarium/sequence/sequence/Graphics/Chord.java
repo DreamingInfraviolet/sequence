5dceadf6646b1e2ed5bd11b20d9b3c292bbbb9a2
Niamh <niamh.oreilly.2014@mumail.ie>
2016-12-02 11:58:43 +0000

    Additions

--- app/src/main/java/com/audinarium/sequence/sequence/Graphics/Chord.java
+++ app/src/main/java/com/audinarium/sequence/sequence/Graphics/Chord.java
@@ -1,5 +1,9 @@
 package com.audinarium.sequence.sequence.Graphics;
 
+import java.util.ArrayList;
+
+import static android.R.attr.name;
+import static android.R.id.list;
 import static android.os.Build.VERSION_CODES.M;
 import static com.audinarium.sequence.sequence.Graphics.Chord.KeyNames.C;
 
@@ -18,16 +22,79 @@ public class Chord{
         soh = soh_;
     }
 
+    public boolean containsKey(KeyNames key)
+    {
+        return doh==key || mi==key || soh==key;
+    }
+
     public enum KeyNames {
         C, CS, D, DS, E, F, FS, G, GS, A, AS, B
     };
 
     public enum ChordNamesMajor{
-        ChordC, ChordCS, ChordD, ChordDS, ChordE,  ChordF, ChordFS, ChordG, ChordGS, ChordA, ChordAS, ChordB
+        ChordC, ChordCS, ChordD, ChordDS, ChordE, ChordF, ChordFS, ChordG, ChordGS, ChordA, ChordAS, ChordB
     };
     public enum ChordNamesMinor{
-        ChordCm, ChordCSm, ChordDm, ChordDSm, ChordEm,  ChordFm, ChordFSm, ChordGm, ChordGSm, ChordAm, ChordASm, ChordBm
+        ChordCm, ChordCSm, ChordDm, ChordDSm, ChordEm, ChordFm, ChordFSm, ChordGm, ChordGSm, ChordAm, ChordASm, ChordBm
     };
+
+    static void ChordOrderMajor()
+    {
+        Chord C = sMajor[ChordNamesMajor.ChordC.ordinal()];
+        Chord Csharp = sMajor[ChordNamesMajor.ChordCS.ordinal()];
+        Chord D = sMajor[ChordNamesMajor.ChordD.ordinal()];
+        Chord Dsharp = sMajor[ChordNamesMajor.ChordDS.ordinal()];
+        Chord E = sMajor[ChordNamesMajor.ChordE.ordinal()];
+        Chord F = sMajor[ChordNamesMajor.ChordF.ordinal()];
+        Chord Fsharp = sMajor[ChordNamesMajor.ChordFS.ordinal()];
+        Chord G = sMajor[ChordNamesMajor.ChordG.ordinal()];
+        Chord Gsharp = sMajor[ChordNamesMajor.ChordGS.ordinal()];
+        Chord A = sMajor[ChordNamesMajor.ChordA.ordinal()];
+        Chord Asharp = sMajor[ChordNamesMajor.ChordAS.ordinal()];
+        Chord B = sMajor[ChordNamesMajor.ChordB.ordinal()];
+    }
+    static void ChordOrderMinor()
+    {
+        Chord Cm = sMinor[ChordNamesMinor.ChordCm.ordinal()];
+        Chord Csharpm = sMinor[ChordNamesMinor.ChordCSm.ordinal()];
+        Chord Dm = sMinor[ChordNamesMinor.ChordDm.ordinal()];
+        Chord Dsharpm = sMinor[ChordNamesMinor.ChordDSm.ordinal()];
+        Chord Em = sMinor[ChordNamesMinor.ChordEm.ordinal()];
+        Chord Fm = sMinor[ChordNamesMinor.ChordFm.ordinal()];
+        Chord Fsharpm = sMinor[ChordNamesMinor.ChordFSm.ordinal()];
+        Chord Gm = sMinor[ChordNamesMinor.ChordGm.ordinal()];
+        Chord Gsharpm = sMinor[ChordNamesMinor.ChordGSm.ordinal()];
+        Chord Am = sMinor[ChordNamesMinor.ChordAm.ordinal()];
+        Chord Asharpm = sMinor[ChordNamesMinor.ChordASm.ordinal()];
+        Chord Bm = sMinor[ChordNamesMinor.ChordBm.ordinal()];
+    }
+
+    public static Chord[] sGetChordsWithKeys(KeyNames[] names)
+    {
+        return sGetChordsWithKeys(names, names.length);
+    }
+
+    /** Returns all chords that contain all of the key names in the chord
+     * @param names An array of keys to search for
+     * @param numberToSucceed Will return a chord if at least numberToSucceed keys are in it.*/
+    public static Chord[] sGetChordsWithKeys(KeyNames[] names, int numberToSucceed)
+    {
+        ArrayList<Chord> output = new ArrayList<>();
+
+        for(Chord c : sMajor)
+        {
+            int successes = 0;
+
+            for(int i = 0; i < names.length; ++i)
+                if(c.containsKey(names[i]))
+                    ++successes;
+
+            if(successes >= numberToSucceed)
+                output.add(c);
+        }
+
+        return output.toArray(new Chord[output.size()]);
+    }
 
     static Chord sMajor[] = new Chord[]
             {
--- app/src/main/java/com/audinarium/sequence/sequence/NotesPlayed.java
+++ app/src/main/java/com/audinarium/sequence/sequence/NotesPlayed.java
@@ -1,8 +1,12 @@
 package com.audinarium.sequence.sequence;
 
 import com.audinarium.sequence.sequence.Graphics.Chord;
+import com.audinarium.sequence.sequence.Graphics.Keyboard;
 
 import java.util.ArrayList;
+
+import static android.os.Build.VERSION_CODES.N;
+import static com.audinarium.sequence.sequence.Graphics.Chord.KeyNames.C;
 
 /**
  * Created by Niamh on 01/12/2016.
@@ -10,14 +14,23 @@ import java.util.ArrayList;
 
 public class NotesPlayed {
 
-    public ArrayList<Chord> recorded(ArrayList<Chord.KeyNames> NotesPlayed)
+
+
+    public ArrayList<Chord> recorded(ArrayList<Chord.KeyNames> notesPlayed)
     {
         ArrayList<Chord> chordOutput = new ArrayList<Chord>();
 
-        for (int i = 0; i < NotesPlayed.size(); i++)
+        for (int i = 0; i < notesPlayed.size(); i++)
         {
-            
-            chordOutput.add(Chord);
+            int nToProcess = notesPlayed.size() - i;
+
+            if(nToProcess > 3)
+            {
+                Chord[] chords0 = Chord.sGetChordsWithKey(notesPlayed.get(i));
+                Chord[] chords1 = Chord.sGetChordsWithKey(notesPlayed.get(i+1));
+
+                Chord[] intersection
+            }
         }
         return chordOutput;
     }
